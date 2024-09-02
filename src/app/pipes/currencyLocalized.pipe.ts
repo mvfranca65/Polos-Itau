1836189453da1c986f9aab315b21e203cce1739e
Marcos Franca <mvfranca65@gmail.com>
2024-09-02 12:32:45 -0300

    feature: Configuracao do paginator para traducao 2

--- src/app/pipes/currencyLocalized.pipe.ts
+++ src/app/pipes/currencyLocalized.pipe.ts
@@ -1,18 +1,20 @@
 import { Pipe, PipeTransform, Inject, LOCALE_ID } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Pipe({
   name: 'currencyLocalized'
 })
 export class CurrencyLocalizedPipe implements PipeTransform {
 
-  constructor(@Inject(LOCALE_ID) private locale: string, private route: ActivatedRoute) {}
+  constructor(@Inject(LOCALE_ID) private locale: string, private route: ActivatedRoute, private router: Router) {}
 
   transform(value: number | string, currencyCode: string = 'BRL'): string {
-    const language = this.route.snapshot.url[0].path; // 'pt'
-    const locale = this.getLocale(language);
-    console.log('LANGUAGE => ', language);
+    const fullPath = this.router.url; // '/pt/itau/home'
+    const language = fullPath.split('/')[1]; // 'pt'
 
+    const locale = this.getLocale(language);
+    console.log('FULLPATH => ', fullPath);
+    console.log('LANGUAGE => ', language);
     console.log("LOCALE => ", locale);
 
     if (typeof value === 'number') {
