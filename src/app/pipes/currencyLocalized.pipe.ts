f55e1e96672aabfac7136df53f065509a038b3ee
Marcos Franca <mvfranca65@gmail.com>
2024-09-02 12:27:41 -0300

    feature: Traducao dos valores do componente Home

--- src/app/pipes/currencyLocalized.pipe.ts
+++ src/app/pipes/currencyLocalized.pipe.ts
@@ -1,17 +1,30 @@
 import { Pipe, PipeTransform, Inject, LOCALE_ID } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
 @Pipe({
   name: 'currencyLocalized'
 })
 export class CurrencyLocalizedPipe implements PipeTransform {
 
-  constructor(@Inject(LOCALE_ID) private locale: string) {}
+  constructor(@Inject(LOCALE_ID) private locale: string, private route: ActivatedRoute) {}
 
   transform(value: number | string, currencyCode: string = 'BRL'): string {
+    const language = this.route.snapshot.url[0].path; // 'pt'
+    const locale = this.getLocale(language);
+    console.log('LANGUAGE => ', language);
+
+    console.log("LOCALE => ", locale);
+
     if (typeof value === 'number') {
-      return value.toLocaleString(this.locale, { style: 'currency', currency: currencyCode });
+      return value.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
     }
     
     return value;
   }
+
+  getLocale = (locale: string) => ({
+    'pt': 'pt-BR',
+    'en': 'en-US',
+    'es': 'es-ES'
+  }[locale] || 'Not Found');
 }
